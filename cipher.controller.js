882e70e9944789bdba964e8f302ce619d120a5bb
Mike Axtman <mdaxtman@gmail.com>
2014-12-16 13:11:23 -0800

    change to 'controller As' syntax

--- cipher.controller.js
+++ cipher.controller.js
@@ -2,42 +2,43 @@
   angular.module('app.controller', [])
     .controller('CipherController', CipherController);
   
-  CipherController.$inject = ['$scope', 'CipherFactory'];
+  CipherController.$inject = ['CipherFactory'];
   
-  function CipherController($scope, CipherFactory){
-    $scope.inputMode = true;
-    $scope.password = CipherFactory.key.join('');
-    $scope.encodeInput = '';
-    $scope.output = '';
-    $scope.encode = CipherFactory.encode;
-    $scope.decode = CipherFactory.decode;
-    $scope.check = check;
-    $scope.change = change;
-    $scope.updatePassword = updatePassword;
+  function CipherController(CipherFactory){
+    var model = this;
+    model.inputMode = true;
+    model.password = CipherFactory.key.join('');
+    model.encodeInput = '';
+    model.output = '';
+    model.encode = CipherFactory.encode;
+    model.decode = CipherFactory.decode;
+    model.check = check;
+    model.change = change;
+    model.updatePassword = updatePassword;
 
     function check() {
-      if($scope.password.length === 0){
-        $scope.output = 'please enter an encryption code';
+      if(model.password.length === 0){
+        model.output = 'please enter an encryption code';
         return false;
       }else{
         return true;
       }
     }
-    
+
     function change(input) {
-      if( $scope.check() ){
-        if($scope.inputMode){
-          $scope.output = $scope.encode.call(CipherFactory, input);  
+      if( model.check() ){
+        if(model.inputMode){
+          model.output = model.encode.call(CipherFactory, input);  
         }else{
-          $scope.output = $scope.decode.call(CipherFactory, input);
+          model.output = model.decode.call(CipherFactory, input);
         }
       } 
     }
 
     function updatePassword() {
-      if( $scope.check() ){
-        CipherFactory.key = $scope.password.toLowerCase().split('');
-        $scope.change($scope.encodeInput);
+      if( model.check() ){
+        CipherFactory.key = model.password.toLowerCase().split('');
+        model.change(model.encodeInput);
       }
     }
   }  
